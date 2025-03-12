61baf31de32cde8ded495d597282efc7ba80319e
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-12 12:53:22 +0600

    Function Value Return add

--- Index.php
+++ Index.php
@@ -42,3 +42,31 @@ $name = "Morshed alam";
 
 print mb_strtoupper($name);
 
+
+echo (10+5) *2;
+echo "<br>";
+function greet($name = "Guest") {
+
+    return "Welcome, " . $name;
+
+    }
+
+   echo greet();
+   echo "<br>";
+   function greet2($name = "Nobody") {
+    return "Hello, " . $name;
+}
+$result= greet2("Alice");
+
+print $result;
+
+echo "<br>";
+
+function greet3() {
+    return "Hello"; 
+}
+$result= greet3();
+
+echo $result;
+
+echo "<br>";
