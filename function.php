796efad5680c70296b79e72e01451e149b06c0ad
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-12 23:01:10 +0600

    for loop use

--- function.php
+++ function.php
@@ -1,1 +1,22 @@
 <?php
+
+function calculator($num){
+   
+    for($i=0; $i<=10; $i++){
+        $result= $num * $i;
+        echo $result . "<br>";
+        
+    }
+   
+}
+ 
+calculator(5);
+
+echo "<br>";
+function forloop(){
+   
+    for($i=0; $i<=15; $i++){
+        echo $i . "<br>";
+    }
+}
+forloop();
