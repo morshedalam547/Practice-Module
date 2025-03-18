22251ac091ab9669cecd8d02ceb9d2ee513e0e60
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-18 12:03:25 +0600

    added

--- add.php
+++ add.php
@@ -39,6 +39,44 @@ if($age > 18){
     echo "she is not a teenager";
 }
 
+$num = 10;
+$num2 = 10;
+
+$result = ($num == $num2) ? "True" : "False";
+
+echo $result;
+
+echo "<br>";
+
+$num = 10;
+$num2 = "10";
+
+$result = ($num === $num2) ? "True" : "False";
+
+echo $result;
+
+echo "<br>";
+
+$number = 10;
+$number2 = 10;
+
+if($number == $number2){
+    echo "True";
+}else{
+    echo "False";
+}
+
+echo "<br>";
+
+$age = 17;
+
+if($age > 18){
+    echo "He is a tenizer";
+
+}else{
+    echo "she is not a teenager";
+}
+
 echo "<br>";
 
 
