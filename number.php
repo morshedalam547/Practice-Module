3a95292e601f12f6716c73ed988b7629c41433b5
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-18 14:18:54 +0600

    php string function

--- number.php
+++ number.php
@@ -22,3 +22,32 @@ echo "<br>";
 $age = 2;
 
 echo ($age >= 13 && $age <= 19) ?? "He is teenager" ?: (($age < 13) ? "He is a kid" : "He is an adult");
+
+
+$string = "Morshed Alam";
+
+echo strlen (string: $string);
+
+//Reverse
+
+echo "<br>";
+echo strrev (string: $string);
+
+echo "<br>";
+
+echo strtolower(string: $string);
+
+echo "<br>";
+
+echo strtoupper(string: $string);
+
+
+echo "<br>";
+
+echo ucwords(string: $string);
+
+echo "<br>";
+$string1 = ' Mahid ';
+$string2 = 'Hossain';
+
+echo trim($string1) . $string2;
