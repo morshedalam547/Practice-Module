041cbb3dc3a3d215662f1419ef371eb1f48d72c5
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-19 20:53:28 +0600

    add

--- Index.php
+++ Index.php
@@ -45,6 +45,78 @@ print mb_strtoupper($name);
 
 echo (10+5) *2;
 echo "<br>";
+// Removed duplicate declaration of the greet function
+
+// Removed duplicate declaration of the greet3 function
+
+$number = 2;
+$number = 2;
+
+if($number >2) {
+    echo "5 is greater than 2";
+}elseif($number ==2) {
+    echo "2 is equal to 2";
+}else {
+    echo "5 is less than 2";
+}
+echo "<br>";
+
+$age = 2;
+
+if($age >=13 && $age  <= 19){
+    echo "He is a teenager";
+
+}elseif($age <= 13){
+    echo "He is a child";
+
+}else{
+    echo "He is an adult";
+}
+$number = 150+150;
+
+echo $number;
+
+$a = 5;
+$b = 6;
+
+$c = $a+$b;
+echo $c;
+
+
+echo "<br>";
+
+$name = "panda";
+
+$name = "panda the great";
+
+print $name;
+
+echo "<br>";
+
+define("name", "panda");
+
+echo name;
+echo "<br>";
+
+define("fname", "panda the great");
+
+echo fname;
+echo "<br>";
+
+$today_date = 6;
+
+$tomorrow_date = date("Y-m-d", strtotime("+2 days"));
+
+print $tomorrow_date;
+echo "<br>";
+
+$name = "Morshed alam";
+
+print mb_strtoupper($name);
+
+
+echo (10+5) *2;
+echo "<br>";
 function greet($name = "Guest") {
 
     return "Welcome, " . $name;
