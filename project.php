300ecca690616b3e757eaf2c47fbe056dc6147d2
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-21 15:02:59 +0600

    add

--- project.php
+++ project.php
@@ -1,2 +1,19 @@
 <?php
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+echo "Hello";
+</body>
+</html>
+
+?>
