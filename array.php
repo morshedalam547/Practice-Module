fc42218fdef7ff409223709a754da4acc871d2de
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-23 12:00:01 +0600

    added

--- array.php
+++ array.php
@@ -300,3 +300,154 @@ if ($average >= 80) {
 myMark2($math, $english, $bangla, $arabic, $history);
 
 
+$marks2=[ 
+    "Morshed"  =>["physics"=> 85,"maths"=> 78,"chemistry"=>89],
+    "Nure Alam"=>["physics"=> 85,"match"=> 95,"chemistry"=>79],
+     "Al amin" =>["physics"=> 95, "match"=> 96, "chemistry"=>79],
+  ];
+
+  foreach($marks2 as $val1){
+    foreach($val1 as $key => $val){
+      echo "Name: $key, Marks: $val <br>";
+    }
+  }
+$marks2=[ 
+    "Morshed"  =>["physics"=> 85,"maths"=> 78,"chemistry"=>89],
+    "Nure Alam"=>["physics"=> 85,"match"=> 95,"chemistry"=>79],
+     "Al amin" =>["physics"=> 95, "match"=> 96, "chemistry"=>79],
+  ];
+
+  foreach($marks2 as $val1){
+    foreach($val1 as $key => $val){
+      echo "Name: $key, Marks: $val <br>";
+    }
+  }
+
+echo "<br>";
+echo "<br>";
+
+
+
+$marks=[ 
+    "Morshed"  =>["physics"=> 85,"maths"=> 78,"chemistry"=>89],
+    "Nure Alam"=>["physics"=> 85,"match"=> 95,"chemistry"=>79],
+     "Al amin" =>["physics"=> 95, "match"=> 96, "chemistry"=>79],
+  ];
+  foreach($marks as $key => $val1){
+   echo $key . " ";
+   foreach($val1 as $val2){
+    echo  $val2 . " ";
+   }
+   echo "<br>";
+  }
+  
+  
+  echo "<br>";
+echo "<br>";
+
+
+  $marks1 = [
+       "Morshed" => ["philosophy"=> 80, "math"=> 90, "english"=> 70, "science"=> 85, "social science"=> 75],
+       "Rahim" => ["philosophy"=> 70, "math"=> 80, "english"=> 60, "science"=> 75, "social science"=> 65],
+          "Karim" => ["philosophy"=> 60, "math"=> 70, "english"=> 50, "science"=> 65, "social science"=> 55]
+  ];
+  
+  foreach ($marks1 as $key => $value) {
+      echo "Name: $key <br>";
+      foreach ($value as $key1 => $value1) {
+          echo "Subject: $key1, Marks: $value1 <br>";
+      }
+      echo "<br>";
+  }
+  foreach ($marks1 as $key => $value) {
+      echo "Name: $key <br>";
+      foreach ($value as $key1 => $value1) {
+          echo "Subject: $key1, Marks: $value1 <br>";
+      }
+      echo "<br>";
+  }
+  
+  $marks=[ 
+    "Morshed"  =>["physics"=> 85,"maths"=> 78,"chemistry"=>89],
+    "Nure Alam"=>["physics"=> 85,"match"=> 95,"chemistry"=>79],
+     "Al amin" =>["physics"=> 95, "match"=> 96, "chemistry"=>79],
+  ];
+  
+  echo "<table border='2px' cellpadding='5px' cellspacing='0'>
+  <tr>
+  <th>Student Name</th>
+  <th>physics</th>
+  <th>match</th>
+  <th>chemistry</th>
+  </tr>";
+  
+  foreach($marks as $key => $v1){
+    echo "<tr>
+    <td>$key</td>" ;
+    foreach($v1 as $v2){
+      echo "<td>$v2</td>";
+    }
+  echo "</tr>";
+  }
+  echo "</table>";
+  
+  echo "<br>";
+  echo "<br>";
+  
+  
+echo "<br>";
+echo "<br>";
+
+
+echo "Math Mark is: ";
+$math = (int) readline();
+
+echo "English Mark is: ";
+$english = (int) readline();
+
+echo "Bangla Mark is: ";
+$bangla = (int) readline();
+
+echo "Arabic Mark is: ";
+$arabic = (int) readline();
+
+echo "History Mark is: ";
+$history = (int) readline();
+
+function myMark($math, $english, $bangla, $arabic, $history) {
+$allMarks = $math + $english + $bangla + $arabic + $history;
+$average = $allMarks / 5;
+
+
+echo "Your Total Marks: $allMarks\\n";
+echo "Your Average Marks: $average\\n";
+
+if ($average >= 80) {
+    echo "You Have Achieved A+\\n";
+} elseif ($average >= 60) {
+    echo "You Have Achieved A-\\n";
+} else {
+    echo "You Are Failed\\n";
+}
+
+
+}
+
+myMark2($math, $english, $bangla, $arabic, $history);
+
+echo "Your Average Marks: $average\\n";
+
+if ($average >= 80) {
+    echo "You Have Achieved A+\\n";
+} elseif ($average >= 60) {
+    echo "You Have Achieved A-\\n";
+} else {
+    echo "You Are Failed\\n";
+}
+
+
+
+
+myMark2($math, $english, $bangla, $arabic, $history);
+
+
