334adee0b60a7e149dbaa18a6591658fddb39776
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-25 23:28:38 +0600

    add

--- oop-Encapsulation.php
+++ oop-Encapsulation.php
@@ -71,67 +71,16 @@ function show(){
 }
 
 
-// $p1 = new person();
+$p1 = new person();
 $p2 = new person("Mahid", 25);
 $p3 = new person("Morshed", 26);
 
-// $p1->show();
+$p1->show();
 $p2->show();
 $p3->show();
 
 echo "<br>";
 echo "<br>";
 
-//inheritance Work under
 
-class player extends person{
-   public $game;
-    
-       public function __construct($name="No Name", $age = 0, $game = "No Game"){
-           parent::__construct($name, $age);
-           $this->game = $game;
-            ;
-        }
-    
-       public function show(){
-            parent::show();
-            echo "Game: $this->game" . "<br>";
-           ;
-        }
-
-
-}
-
-
-$player1 = new player("Mahid", 25, "Cricket") ;
-$player1->show();
-
-$player2 = new player("Morshed", 26, "Football") ;
-$player2->show();
-
-
-
-class Animal{
-
-    public function sound(){
-        echo "This is Animal Sound" . "<br>";
-    }
-}
-
-echo "<br>";
-
-
-class Dog extends Animal{
-
-    public function __construct(){             // akene constructor use korar krone ait sober age call hobe.
-        echo "Dog class is create" . "<br>";
-    }
-    public function playing(){
-        echo "Dog is playing";
-    }
-}
-
-$dog = new Dog();
-
-echo $dog->sound();
-echo $dog->playing();
+//Abstract Class Work under
