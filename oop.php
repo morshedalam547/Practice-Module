ef40a9cea7a4e690a08d4f0cebf93c6df1383614
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-25 23:24:35 +0600

    Get students

--- oop.php
+++ oop.php
@@ -23,28 +23,27 @@ class student{
 
 
 $student1 = new student;
-$student1 -> name = "Mahid";
-$student1 -> roll = 1;
-$student1 -> class = "Programmer";
-$student1 -> showResult();
+
 $student1 -> getStudentInfo("Mahid", 1, "Programmer");
+$student1 -> showResult();
 
 echo "<br>";
 echo "<br>";
 
 
 $student2 = new student;
-$student2 -> name = "Morshed";
-$student2 -> roll = 2;
-$student2 -> class = "Programmer";
-$student2 -> showResult();
+
 $student2 -> getStudentInfo("Morshed", 2, "Programmer");
+$student2 -> showResult();
 
 echo "<br>";
 echo "<br>";
 
-$student3 = new student("Maimuna", 4, "Programmer");
+$student3 = new student;
+
+$student3 -> getStudentInfo("Maimuna", 3, "Programmer");
 $student3 -> showResult();
+
 
 echo "<br>";
 echo "<br>";
