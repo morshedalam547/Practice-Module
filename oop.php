2583b78c95c37ab9c27928cbae710c099341195f
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-25 23:18:23 +0600

    add

--- oop.php
+++ oop.php
@@ -109,3 +109,30 @@ $player1->show();
 
 $player2 = new player("Morshed", 26, "Football") ;
 $player2->show();
+
+
+
+class Animal{
+
+    public function sound(){
+        echo "This is Animal Sound" . "<br>";
+    }
+}
+
+echo "<br>";
+
+
+class Dog extends Animal{
+
+    public function __construct(){             // akene constructor use korar krone ait sober age call hobe.
+        echo "Dog class is create" . "<br>";
+    }
+    public function playing(){
+        echo "Dog is playing";
+    }
+}
+
+$dog = new Dog();
+
+echo $dog->sound();
+echo $dog->playing();
